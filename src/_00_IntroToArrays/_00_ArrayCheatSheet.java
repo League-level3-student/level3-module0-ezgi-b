f8b99d585a52e0388a7950dc74cdbb6b3a7acd28
ezgi-b <52934686+ezgi-b@users.noreply.github.com>
2020-11-16 20:01:00 -0800

    I finished level 3 module 0

--- src/_00_IntroToArrays/_00_ArrayCheatSheet.java
+++ src/_00_IntroToArrays/_00_ArrayCheatSheet.java
@@ -4,6 +4,82 @@ import java.util.Random;
 
 public class _00_ArrayCheatSheet {
 	public static void main(String[] args) {
+		
+		String[] strings = new String[5];
+		
+		strings[0] = "jaffy";
+		strings[1] = "ilikanna";
+		strings[2] = "meridoli";
+		strings[3] = "oleoyd";
+		strings[4] = "kejrek";
+		
+		
+		System.out.println(strings[2]);
+		
+		strings[2] = "lala";
+		
+		System.out.println(strings[2]);
+		
+		for(int i = 0; i < strings.length; i++) {
+			System.out.println(strings[i]);
+		}
+		
+		for(int i = 0; i < strings.length; i++) {
+			strings[i] = "dkleeoe" + (i + 1);
+		}
+		for(int i = 0; i < strings.length; i++) {
+			System.out.println(strings[i]);
+		}
+		
+		int x[] = new int[50];
+		
+		Random r = new Random();
+		
+		for(int i = 0; i < x.length; i++) {
+			x[i] = r.nextInt(101)-50;
+		}
+		
+		int least = 13;
+		
+		for(int i = 0; i < x.length; i++) {
+			if(i==0) {
+				least = x[i];
+			}else if (x[i]<least) {
+				least = x[i];
+			}
+		}
+		
+		System.out.println(least);
+		System.out.println("");
+		
+		for(int i = 0; i < x.length; i++) {
+			System.out.println(x[i]);
+		}
+		
+		System.out.println("");
+		
+		int greatest = 13;
+		
+		for(int i = 0; i < x.length; i++) {
+			if(i==0) {
+				greatest = x[i];
+			}else if (x[i]>greatest) {
+				greatest = x[i];
+			}
+		}
+		
+		System.out.println(greatest);
+		System.out.println("");
+		
+		for(int i = 0; i < x.length; i++) {
+			System.out.println(x[i]);
+		}
+		
+		System.out.println("");
+		System.out.println(x[49]);
+		System.out.println(x[x.length-1]);
+		
+		
 		//1. make an array of 5 Strings
 
 		//2. print the third element in the array
